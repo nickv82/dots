9ea15fc92949c3931fdcfa99631c5eadaf4250e7
npiv <nick.verlinde@gmail.com>
2013-06-24 14:23:16 +0200

    fixed broken history

--- zsh/config/setopt.zsh
+++ zsh/config/setopt.zsh
@@ -1,42 +1,25 @@
-# ===== Basics
-setopt no_beep # don't beep on error
-setopt interactive_comments # Allow comments even in interactive shells (especially for Muness)
+HISTFILE=~/.zsh_history
+HISTSIZE=10000
+SAVEHIST=10000
 
-# ===== Changing Directories
-setopt auto_cd # If you type foo, and it isn't a command, and it is a directory in your cdpath, go there
-setopt cdablevarS # if argument to cd is the name of a parameter whose value is a valid directory, it will become the current directory
-setopt pushd_ignore_dups # don't push multiple copies of the same directory onto the directory stack
+setopt NO_BG_NICE # don't nice background tasks
+setopt NO_HUP
+setopt NO_LIST_BEEP
+setopt LOCAL_OPTIONS # allow functions to have local options
+setopt LOCAL_TRAPS # allow functions to have local traps
+setopt HIST_VERIFY
+setopt SHARE_HISTORY # share history between sessions ???
+setopt EXTENDED_HISTORY # add timestamps to history
+setopt PROMPT_SUBST
+setopt CORRECT
+setopt COMPLETE_IN_WORD
+setopt IGNORE_EOF
 
-# ===== Expansion and Globbing
-setopt extended_glob # treat #, ~, and ^ as part of patterns for filename generation
+setopt APPEND_HISTORY # adds history
+setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
+setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
+setopt HIST_REDUCE_BLANKS
 
-# ===== History
-setopt append_history # Allow multiple terminal sessions to all append to one zsh command history
-setopt extended_history # save timestamp of command and duration
-setopt inc_append_history # Add comamnds as they are typed, don't wait until shell exit
-#setopt hist_expire_dups_first # when trimming history, lose oldest duplicates first
-#setopt hist_ignore_dups # Do not write events to history that are duplicates of previous events
-#setopt hist_ignore_space # remove command line from history list when first character on the line is a space
-#setopt hist_find_no_dups # When searching history don't display results already cycled through twice
-#setopt hist_reduce_blanks # Remove extra blanks from each command line being added to history
-#setopt hist_verify # don't execute, just expand history
-setopt share_history # imports new commands and appends typed commands to history
-
-# ===== Completion 
-setopt always_to_end # When completing from the middle of a word, move the cursor to the end of the word    
-setopt auto_menu # show completion menu on successive tab press. needs unsetop menu_complete to work
-setopt auto_name_dirs # any parameter that is set to the absolute name of a directory immediately becomes a name for that directory
-setopt complete_in_word # Allow completion from within a word/phrase
-
-unsetopt menu_complete # do not autoselect the first completion entry
-
-# ===== Correction
-setopt correct # spelling correction for commands
-setopt correctall # spelling correction for arguments
-
-# ===== Prompt
-setopt prompt_subst # Enable parameter expansion, command substitution, and arithmetic expansion in the prompt
-setopt transient_rprompt # only show the rprompt on the current prompt
-
-# ===== Scripts and Functions
-setopt multios # perform implicit tees or cats when multiple redirections are attempted
+# don't expand aliases _before_ completion has finished
+#   like: git comm-[tab]
+setopt complete_aliases

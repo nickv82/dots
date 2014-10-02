e52f6c103b4409165659a4e62450ad8a1c8c24d2
Nick Verlinde <nick.verlinde@gmail.com>
2014-10-02 10:17:51 +0100

    some bt aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -18,3 +18,10 @@ alias count_commits=" git log | grep commit | wc -l"
 alias br=" echo "----------------------------------------------------------------""
 
 alias cdd="cd $ZSH"
+
+alias bton="cp ~/.m2/settings.xml.bak ~/.m2/settings.xml"
+alias btoff="rm ~/.m2/settings.xml"
+
+alias m2on="export MAVEN_OPTS=\"-XX:+UseConcMarkSweepGC -XX:+CMSPermGenSweepingEnabled -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=128m -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005\""
+alias m2off="unset MAVEN_OPTS"
+

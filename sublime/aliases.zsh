f3157d82f10677c83042fe4e385f9e3dfff26a95
Nick Verlinde <npiv@Nicks-MacBook-Pro.local>
2017-06-29 22:12:46 +0100

    remove more

--- sublime/aliases.zsh
+++
@@ -1,5 +0,0 @@
-if [[ -n "$IS_LINUX" ]]; then
-    SUB_HOME="~/.config/sublime-text-3/Packages/User/"
-    alias slocal="cp Preferences.sublime-settings $SUB_HOME"
-    alias sremote="cp $SUB_HOME/Preferences.sublime-settings ~/dots/sublime/"
-fi
